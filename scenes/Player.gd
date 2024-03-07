26354888022252d00f64dd4ab6e14a8fd0c4b9c0
yesseruser <yesseruseryt@gmail.com>
2024-03-07 19:49:21 +0100

    Improve code

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 
+const UP = Vector2.UP
 
-const SPEED = 400.0
-const STOP_STEP = 40.0
-const JUMP_VELOCITY = -800.0
-@onready var sprite_2d = $Sprite2D
+@export_category("Movement")
+@export var speed := 400.0
+@export var stop_step := 40.0
+@export var jump_velocity := -800.0
+@export var sprite_2d: AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, stop_step)
+	
 	if (abs(velocity.x) > 1):
 		sprite_2d.animation = "running"
 	else:
@@ -23,15 +30,7 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, STOP_STEP)
+		velocity.y = jump_velocity
 
 	move_and_slide()
 	
