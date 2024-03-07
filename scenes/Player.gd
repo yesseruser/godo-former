389e0401c701428c07270776597d65839fe37471
yesseruser <yesseruseryt@gmail.com>
2024-03-07 18:39:16 +0100

    Tweak player movement + use animations

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,17 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
+const SPEED = 400.0
+const STOP_STEP = 40.0
+const JUMP_VELOCITY = -800.0
+@onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	if (abs(velocity.x) > 1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,6 +31,11 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, STOP_STEP)
 
 	move_and_slide()
+	
+	if velocity.x != 0: # Make player stay facing left if they stopped.
+		var isLeft = velocity.x < 0
+		sprite_2d.flip_h = isLeft
+	
